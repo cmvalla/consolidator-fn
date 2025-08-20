ddb78cccb90aa7a1c9fdd1242fc0b3d31a8a56d6
c.valla <c.valla@goreply.io>
2025-08-20 11:12:58 +0200

    feat(consolidator): update backend.tf and cloudbuilds for project suffix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "terraform-goreply-devops-tools"
-    prefix = "cloud-spanner-demo/consolidator"
+    prefix = "cloud-spanner-demo/consolidator/${terraform.workspace}"
   }
 }
