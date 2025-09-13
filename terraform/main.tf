0af28686a3ac57d3729dfd0fc5da18d35d2ed114
c.valla <c.valla@goreply.io>
2025-09-13 18:39:28 +0200

    f

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "google_cloud_run_v2_service" "consolidator" {
     timeout         = "1800s" # 30 minutes for potentially long-running consolidations
     scaling {
       min_instance_count = 0 # Can scale to zero
-      max_instance_count = 10
+      max_instance_count = 1
     }
     max_instance_request_concurrency = 1
     vpc_access {
